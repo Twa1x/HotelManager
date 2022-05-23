513fa40e26cdf71e5c3e4eaf75bc3b4e8b3cef03
Calcioiu Alexandru <77210765+Twa1x@users.noreply.github.com>
2022-05-23 23:26:43 +0300

    Updates on diffrent functions

--- HotelManager/HotelManager/ViewModels/GuestViewModel.cs
+++ HotelManager/HotelManager/ViewModels/GuestViewModel.cs
@@ -14,6 +14,22 @@ namespace HotelManager.ViewModels
     {
         private int count;
 
+        private String dateStart;
+        private String dateEnd;
+
+        public String DateStart
+        {
+            get { return dateStart; }
+            set { OnPropertyChanged(ref dateStart, value); }
+        }
+
+        public String DateEnd
+        {
+            get { return dateEnd; }
+            set { OnPropertyChanged(ref dateEnd, value); }
+        }
+
+
 
         private RoomModel currentRoomGuest;
         public RoomModel CurrentRoomGuest
@@ -23,17 +39,72 @@ namespace HotelManager.ViewModels
         }
 
         public ObservableCollection<RoomModel> RoomsGuest { get; set; }
+
         public ICommand CheckRoomsCommand { get; }
         public ICommand DetailRoomCommand { get; }
+
+        public ICommand SearchCommand { get; }
         public GuestViewModel()
         {
             RoomsGuest = LoadRooms();
             CheckRoomsCommand = new RelayCommand(CheckRooms);
             DetailRoomCommand = new RelayCommand(DetailRoom);
+            SearchCommand = new RelayCommand(Search);
+        }
+
+
+        private void Search()
+        {
+            String stringHelp = string.Empty;
+            HotelEntities hotelEntities = new HotelEntities();
+            List<Reservation> reservationList = hotelEntities.Reservations.ToList();
+            List<Room> roomsList = hotelEntities.Rooms.ToList();
+
+            List<int> roomsIds = new List<int>();
+            foreach (var item in reservationList)
+            {
+
+                if (((Convert.ToDateTime(item.date_start) <= Convert.ToDateTime(dateStart)) && Convert.ToDateTime(item.date_end) >= Convert.ToDateTime(dateEnd))
+                    || (Convert.ToDateTime(item.date_start) <= Convert.ToDateTime(dateStart)) && Convert.ToDateTime(item.date_end) >= Convert.ToDateTime(dateEnd)
+                    || ((Convert.ToDateTime(item.date_start) >= Convert.ToDateTime(dateStart)) && Convert.ToDateTime(item.date_end) >= Convert.ToDateTime(dateEnd)) && Convert.ToDateTime(item.date_start) <= Convert.ToDateTime(dateEnd)
+                    || ((Convert.ToDateTime(item.date_start) >= Convert.ToDateTime(dateStart)) && Convert.ToDateTime(item.date_end) <= Convert.ToDateTime(dateEnd)))
+                {
+                    Console.WriteLine("nu");
+                    roomsIds.Add(Convert.ToInt32(item.id_room));
+
+                }
+                else
+                {
+                    Console.WriteLine("da");
+                }
+            }
+
+            foreach (var item in roomsList)
+            {
+                bool ok = false;
+
+                foreach (var tempRoom in roomsIds)
+                {
+                    if (item.id_room == tempRoom)
+                    {
+                        ok = true;
+                        break;
+                    }
+                }
+                if (!ok)
+                {
+                    if (item.deleted == 0 || item.deleted == null || item.availabilty == 1)
+                       stringHelp += ($"Room type: { item.type}, price: {item.price}, aditional services: {item.aditional_services}\n") ;
+                    //Convert.ToDouble((Convert.ToDateTime(dateEnd)-Convert.ToDateTime(dateStart)
+                }
+            }
+            MessageBox.Show(stringHelp);
+
+
         }
 
         private void DetailRoom()
-        { 
+        {
 
             DetailsRoomWindow detailsRoomWindow = new DetailsRoomWindow();
             detailsRoomWindow.DataContext = this;
@@ -47,7 +118,7 @@ namespace HotelManager.ViewModels
             List<Room> rooms = hotelEntities.Rooms.ToList();
             foreach (Room room in rooms)
             {
-                if (room.availabilty != 0 && (room.deleted == 0 || room.deleted == null) )
+                if (room.availabilty != 0 && (room.deleted == 0 || room.deleted == null))
                     count++;
             }
 

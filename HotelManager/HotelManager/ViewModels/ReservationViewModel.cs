e0e7a0b183a7ee158846a42975ea8b61199a6275
Calcioiu Alexandru <77210765+Twa1x@users.noreply.github.com>
2022-05-25 16:31:48 +0300

    Last Update( reservations made)
    hopefully

--- HotelManager/HotelManager/ViewModels/ReservationViewModel.cs
+++ HotelManager/HotelManager/ViewModels/ReservationViewModel.cs
@@ -5,6 +5,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace HotelManager.ViewModels
@@ -13,6 +14,8 @@ namespace HotelManager.ViewModels
     {
         private ReservationModel currentReservation;
 
+        private ReservationModel reservationModel;
+        
         private HotelEntities hotelEntities = new HotelEntities();
         public ReservationModel CurrentReservation
         {
@@ -20,18 +23,26 @@ namespace HotelManager.ViewModels
             set { OnPropertyChanged(ref currentReservation, value); }
         }
 
+        public ReservationModel ReservationModel
+        {
+            get { return reservationModel; }
+            set { OnPropertyChanged(ref reservationModel, value); }
+        }
         public ObservableCollection<ReservationModel> Reservations { get; set; }
 
         public ICommand ActiveCommand { get; }
         public ICommand PaidCommand { get; }
         public ICommand CanceledCommand { get; }
+        public ICommand BookCommand { get; }
+
         public ReservationViewModel()
         {
             Reservations = LoadReservations();
-
+            ReservationModel = new ReservationModel();
             ActiveCommand = new RelayCommand(Active);
             PaidCommand = new RelayCommand(Paid);
             CanceledCommand = new RelayCommand(Cancel);
+              BookCommand = new RelayCommand(Book);
         }
 
         private void Cancel()
@@ -50,6 +61,70 @@ namespace HotelManager.ViewModels
         {
            
             hotelEntities.sp_update_reservations(currentReservation.Id, "active");
+        }
+
+        private void Book()
+        {
+            int count = 0;
+
+            HotelEntities hotelEntities = new HotelEntities();
+            List<Reservation> reservationList = hotelEntities.Reservations.ToList();
+            List<Room> roomsList = hotelEntities.Rooms.ToList();
+
+            List<int> roomsIds = new List<int>();
+            foreach (var item in reservationList)
+            {
+
+                if (((Convert.ToDateTime(item.date_start) <= Convert.ToDateTime(ReservationModel.DateStart)) && Convert.ToDateTime(item.date_end) >= Convert.ToDateTime(ReservationModel.DateEnd))
+                    || (Convert.ToDateTime(item.date_start) <= Convert.ToDateTime(ReservationModel.DateStart)) && Convert.ToDateTime(item.date_end) >= Convert.ToDateTime(ReservationModel.DateEnd)
+                    || ((Convert.ToDateTime(item.date_start) >= Convert.ToDateTime(ReservationModel.DateStart)) && Convert.ToDateTime(item.date_end) >= Convert.ToDateTime(ReservationModel.DateEnd)) && Convert.ToDateTime(item.date_start) <= Convert.ToDateTime(reservationModel.DateEnd)
+                    || ((Convert.ToDateTime(item.date_start) >= Convert.ToDateTime(ReservationModel.DateStart)) && Convert.ToDateTime(item.date_end) <= Convert.ToDateTime(ReservationModel.DateEnd)))
+                {
+                    Console.WriteLine("nu");
+                    roomsIds.Add(Convert.ToInt32(item.id_room));
+
+                }
+                else
+                {
+                    count++;
+                }
+            }
+
+            if (count != 0)
+            {
+
+                foreach (var item in roomsList)
+                {
+                    bool ok = false;
+
+                    foreach (var tempRoom in roomsIds)
+                    {
+                        if (item.id_room == tempRoom)
+                        {
+                            ok = true;
+                            break;
+                        }
+                    }
+                    if (!ok)
+                    {
+                        if (item.deleted == 0 || item.deleted == null || item.availabilty == 1 && item.type == reservationModel.RoomType)
+                        {
+
+                            int numberOfDays = Convert.ToInt32(((Convert.ToDateTime(ReservationModel.DateEnd) - Convert.ToDateTime(ReservationModel.DateStart))).TotalDays);
+                            double price = item.price * numberOfDays;
+
+                            hotelEntities.sp_insert_reservation(Convert.ToDateTime(ReservationModel.DateStart), Convert.ToDateTime(ReservationModel.DateEnd), "active", item.id_room, 5, null, price, 0);
+                            MessageBox.Show("Booked!");
+                            break;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("No rooms available!");
+            }
+
         }
 
         private ObservableCollection<ReservationModel> LoadReservations()
